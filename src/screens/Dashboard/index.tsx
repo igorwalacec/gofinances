f433f45feb397b3ff15b203092dbf4267bfe5eb5
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2021-12-31 00:51:44 -0300

    Add TransactionCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
     Container, 
@@ -11,10 +12,52 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionsList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+    {
+        id: '1',
+        type: 'positive',
+        title:"Desenvolvimento de site",
+        amount:"R$ 12.000,00",
+        category:{
+            name: 'Vendas',
+            icon: 'dollar-sign',
+        },
+        date:"13/04/2021"
+    },
+    {
+        id: '2',
+        type: 'negative',
+        title:"Almoço",
+        amount:"R$ 100,00",
+        category:{
+            name: 'Vendas',
+            icon: 'coffee',
+        },
+        date:"13/04/2021"
+    },
+    {
+        id: '3',
+        type: 'negative',
+        title:"Compra online",
+        amount:"R$ 400,00",
+        category:{
+            name: 'Vendas',
+            icon: 'shopping-bag',
+        },
+        date:"13/04/2021"
+    }
+    ];
     return(
         <Container>        
             <Header>
@@ -50,6 +93,16 @@ export function Dashboard() {
                     type="total"
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionsList 
+                    data={data}  
+                    keyExtractor={item => item.id}
+                    renderItem={({item})=> <TransactionCard data={item}/>}                 
+                />
+            </Transactions>
         </Container>
     ) 
 };
